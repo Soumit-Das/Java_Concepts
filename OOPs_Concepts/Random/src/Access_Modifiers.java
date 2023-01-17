9b86c814e8dee27c92cb2995ffc1132212a11771
Soumit-Das <soumitdas500@gmail.com>
2023-01-17 08:15:20 +0530

    Access modifiers concept

---
+++ OOPs_Concepts/Random/src/Access_Modifiers.java
@@ -0,0 +1,42 @@
+class Launch
+{
+    Aaa obj=new Aaa() {
+    };
+	System.out.println(obj.marks);
+
+
+}
+
+
+public class B
+{
+    //	private int marks;
+    int marks;
+
+}
+
+public static class Aaa {
+    int marks = 6;
+
+    //	protected marks=6;
+    public void show() {
+
+    }
+}
+
+
+class C extends Aaa
+{
+    public void abc()
+    {
+        System.out.println(marks);
+    }
+}
+
+public class Access_Modifiers {
+
+    public static void main(String[] args) {
+
+    }
+
+}
