25fd773f941d4590280bbdf09fed5e4e8e37a1a8
Soumit-Das <soumitdas500@gmail.com>
2023-01-28 11:26:20 +0530

    Assignment  operators concept

---
+++ OOPs_Concepts/Random/src/Assignment_Operators.java
@@ -0,0 +1,55 @@
+
+public class Assignment_Operators {
+
+    public static void main(String[] args) {
+/**		int num1=7;
+ int num2=5;
+ int result=num1+num2;
+ System.out.println(result);
+ **/
+
+/**
+ int num1=7;
+ int num2=5;
+ int result=num1-num2;
+ System.out.println(result);
+ **/
+
+/**
+ int num1=7;
+ int num2=5;
+ int result=num1*num2;
+ System.out.println(result);
+ **/
+
+/**
+ int num1=7;
+ int num2=5;
+ int result=num1/num2;
+ System.out.println(result);
+ **/
+
+/**
+ int num1=7;
+ int num2=5;
+ int result=num1%num2;
+ System.out.println(result);
+ **/
+
+        int num=7;
+//		num=num+2;
+//		num+=2;
+//		num*=2;
+
+        num++;    //post increment
+        ++num;    //pre increment
+        num--;    //post decrement
+        --num;	  //pre decrement
+        System.out.println(num);
+
+        int result=num++; //fetch the value and then increment
+        System.out.println(result);
+
+    }
+
+}
