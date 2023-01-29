c76b162a90b72cabb26fc2eae04339006bf7b328
Soumit-Das <soumitdas500@gmail.com>
2023-01-29 12:16:20 +0530

    Checked Exception concept

---
+++ OOPs_Concepts/Random/src/Checked_Exception_Finally_block.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// BufferReader
+// Scanner
+
+public class Checked_Exception_Finally_block
+{
+    public static void main(String[] args) throws Exception
+    {
+        int n=0;
+        System.out.println("Enter a number");
+
+        BufferedReader br = null;
+        try
+        {
+
+            br = new BufferedReader(new InputStreamReader(System.in));
+            n = Integer.parseInt(br.readLine());  // 45
+            br.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            br.close();
+        }
+
+        System.out.println(n);
+    }
+}
