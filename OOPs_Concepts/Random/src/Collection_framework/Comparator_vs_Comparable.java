56bdc45add28be7640a9f39f45a408ceba499bf3
Soumit-Das <soumitdas500@gmail.com>
2023-04-09 10:20:20 +0530

    Comparator vs Comparable concept

---
+++ OOPs_Concepts/Random/src/Collection_framework/Comparator_vs_Comparable.java
@@ -0,0 +1,92 @@
+package Collection_framework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
+
+
+//class Student implements Comparable<Student>
+class Student
+{
+    int age;
+    String name;
+
+    public Student(int age, String name)
+    {
+        this.age=age;
+        this.name=name;
+    }
+
+    public String toString() {
+        return "Student [age=" + age + ", name=" +name +"]";
+    }
+
+//	public int CompareTo(Student that)
+//	{
+////		return 0;
+//		if(this.age >that.age)
+//			return 1;
+//		else
+//			return -1;
+//	}
+}
+
+
+public class Comparator_vs_Comparable {
+    public static void main(String[] args){
+
+//    	Comparator<Integer> com=new Comparator<Integer>()
+//    	{
+//    		public int compare(Integer i,Integer j)
+//    		{
+//    			if(i%10 >j%10)
+//    				return 1;
+//    			else
+//    				return -1;
+//    		}
+//    	};
+
+//    	List<Integer> nums= new ArrayList<>();
+//    	nums.add(43);
+//    	nums.add(31);
+//    	nums.add(72);
+//    	nums.add(29);
+
+//    	Comparator<Student> com=new Comparator<Student>()
+//    	{
+//    		public int compare(Student i,Student j)
+//    		{
+//    			if(i.age >j.age)
+//    				return 1;
+//    			else
+//    				return -1;
+//    		}
+//    	};
+
+        Comparator<Student> com=(i,j) -> i.age > j.age?1:-1;
+
+        List<Student> studss= new ArrayList<>();
+        studss.add(new Student(21,"Navin"));
+        studss.add(new Student(12,"John"));
+        studss.add(new Student(18,"Parul"));
+        studss.add(new Student(20,"Kiran"));
+
+//    	Collections.sort(nums);
+//    	System.out.println(nums);
+
+        for(Student s:studss)
+            System.out.println();
+
+        Collections.sort(studss);
+        for(Student s: studss)
+            System.out.println(s);
+    }
+}
+
+
+
+
+
+
+
