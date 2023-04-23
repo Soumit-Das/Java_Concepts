b89af34b6b2971d3fda74a5dfbdf426b8a0071ae
Soumit-Das <soumitdas500@gmail.com>
2023-04-23 10:15:20 +0530

    Do while loop concept

---
+++ OOPs_Concepts/Random/src/Collection_framework/Do_While_Loop_Concepts.java
@@ -0,0 +1,15 @@
+package Collection_framework;
+
+
+public class Do_While_Loop_Concepts {
+
+    public static void main(String[] args) {
+        int i=1;
+        do
+        {
+            System.out.println("Hi"+i);
+            i++;
+        }
+        while(i<=4);
+    }
+}
