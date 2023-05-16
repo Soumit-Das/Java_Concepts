a1a65fd00bcd6d8f1fe8ddf54858feb7c15b3664
Soumit-Das <soumitdas500@gmail.com>
2023-05-16 11:23:20 +0530

    final keyword concept

---
+++ OOPs_Concepts/Random/src/Final_keyword.java
@@ -0,0 +1,51 @@
+
+//final - variable, method, class
+
+//final class Calc
+//{
+//	public void show()
+//	{
+//		System.out.println("in Calc show");
+//	}
+//	public void add(int a, int b)
+//	{
+//		System.out.println(a+b);
+//	}
+//}
+
+
+class Calc
+{
+    public void show()
+    {
+        System.out.println("By Navin");
+    }
+    public void add(int a, int b)
+    {
+        System.out.println(a+b);
+    }
+}
+
+class AdvCalc extends Calc {
+    public void show() {
+        System.out.println("By John");
+
+    }
+}
+
+    public class  Final_keyword {
+        public static void main(String[] args) {
+
+//    	final int num=8;
+//    	num=9;
+//    	System.out.println(num);
+
+//    	Calc obj= new Calc();
+//    	obj.show();
+//    	obj.add(4, 5);
+
+            AdvCalc obj = new AdvCalc();
+            obj.show();
+            obj.add(4, 5);
+        }
+    }
