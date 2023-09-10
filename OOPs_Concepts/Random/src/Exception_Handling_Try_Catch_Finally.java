b3a4b711aa81f7648f8db131122454b568076e7f
Soumit-Das <soumitdas500@gmail.com>
2023-09-10 13:34:41 +0530

    try catch finally concept

---
+++ OOPs_Concepts/Random/src/Exception_Handling_Try_Catch_Finally.java
@@ -0,0 +1,26 @@
+
+
+public class Exception_Handling_Try_Catch_Finally
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            int i = 7;
+            int j = 2;
+            int k=i/j;
+            System.out.println("output is " + k);
+//			System.out.println("Bye");
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Error ");
+//			System.out.println("Bye");
+        }
+        finally
+        {
+            System.out.println("Bye");
+        }
+
+    }
+}
